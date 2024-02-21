e10e5594e98b6252f9b9c96379ae14cc7d4b15ef
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-21 06:49:57 -0500

    getting rid basename in app

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router basename={'/example_Two'}>
+        <Router>
             <MyNav />
             <Routes>
                 <Route path='' element={<Home />} />
