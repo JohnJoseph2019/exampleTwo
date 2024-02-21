35cfd8b750332bf16b696e7b800fea47db3fb38d
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-21 06:56:34 -0500

    adding basename in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router>
+        <Router basename='/exampleTwo/'>
             <MyNav />
             <Routes>
                 <Route path='/' element={<Home />} />
