bb6dcc4477385a28532f87a9e1343baf03e36368
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-21 05:52:28 -0500

    starting with the new code

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/example_Two',
+    build: {
+        outDir: 'build',
+    },
+    plugins: [react()],
+});
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
-//   base: '/react-portfolio-andrea-melisa',
-//   build: {
-//     outDir: 'build'
-//   },
 //   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
 // })
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
