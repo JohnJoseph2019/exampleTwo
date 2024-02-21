d94bd8d87f28c07f5640a5fe2fd2a276e0df3058
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-21 06:30:45 -0500

    changes 5

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+// import {defineConfig} from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/',
-    build: {
-        outDir: 'build',
-    },
-    plugins: [react()],
-});
+// export default defineConfig({
+//     base: '/',
+//     build: {
+//         outDir: 'build',
+//     },
+//     plugins: [react()],
+// });
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
@@ -21,3 +21,21 @@ export default defineConfig({
 //     open: true
 //   }
 // })
+
+// vite.config.js
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({command}) => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+    };
+
+    if (command !== 'serve') {
+        config.base = '/example_Two';
+    }
+
+    return config;
+});
